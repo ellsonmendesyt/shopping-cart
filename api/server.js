36464f3befe0fe26fe8f0e2a32abda3192b57c05
ellsonmendesyt <elsonmendesoficial@gmail.com>
2022-03-17 09:24:19 -0300

    glassy overlay imrovement

--- api/server.js
+++ api/server.js
@@ -3,26 +3,17 @@ const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults({static: 'public'})
 const port=4000;
-// Set default middlewares (logger, static, cors and no-cache)
+ 
 server.use(middlewares)
 
-// Add custom routes before JSON Server router
+
 server.get('/echo', (req, res) => {
   res.jsonp(req.query)
 })
 
-// To handle POST, PUT and PATCH you need to use a body-parser
-// You can use the one used by JSON Server
-server.use(jsonServer.bodyParser)
-// server.use((req, res, next) => {
-//   if (req.method === 'POST') {
-//     req.body.createdAt = Date.now()
-//   }
-//   // Continue to JSON Server router
-//   next()
-// })
 
-// Use default router
+server.use(jsonServer.bodyParser)
+
 server.use(router)
 server.listen(port, () => {
   console.log('JSON Server is running')
